064d83bc14da29613fa5da848c338191a59a95b9
Scott Petersen <petersen@centtech.com>
2018-09-02 22:12:33 -0500

    fixing __future__ import

--- button-control.py
+++ button-control.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+import sys
+
 import RPi.GPIO as GPIO
 import time
 from chromatron import *
 
-from __future__ import print_function
-import sys
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
